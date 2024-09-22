85b6d0a881d1b25583f79b3d44aed5fe211cee61
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-22 13:56:36 +0530

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import  { useState, useEffect } from 'react';
 import axios from 'axios';
-import "./app.css"
+import "./App.css"
 
 const App = () => {
   const [userData, setUserData] = useState(null);
