a1e6df209971fdef3d30682aa37b9f8b2368bc90
Pritesh Patel <pritbusiness03@gmail.com>
2024-09-24 21:51:57 +0530

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://server-1-aopt.onrender.com/',
-        secure: false, 
+        changeOrigin:true,
         agent: new https.Agent({
           rejectUnauthorized: false
         })
